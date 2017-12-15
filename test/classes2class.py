e26e541c475fa0e70f5f1ef6f8e0e190c55a4196
tpostadjian <tristanpostadjian@gmail.com>
2017-12-15 17:43:30 +0000

    Merge branch 'stable' of https://github.com/tpostadjian/pytorch into stable
    Conflicts:
    	test/pred4seg.py

--- test/classes2class.py
+++ test/classes2class.py
@@ -2,26 +2,26 @@ import numpy as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', help = 'prediction over SSPs')
-parser.add_argument('-o', help = 'output merged predictions')
+parser.add_argument('-i', help='prediction over SSPs')
+parser.add_argument('-o', help='output merged predictions')
 args = parser.parse_args()
 
 f_txt = np.loadtxt(args.i)
 n_seg = f_txt.max()
 f_out = args.o
 
-out = open(f_out,'w')
+out = open(f_out, 'w')
 
 for i in range(int(n_seg)):
-	ind_pixs = np.where(f_txt[:,0]==i+1)
-	pixs = f_txt[ind_pixs][:,1:6]
-	n_pixs = pixs.shape[0]
-	if n_pixs==0:
-		continue
-	else:
-		proba_seg = np.sum(pixs, axis=0)/n_pixs
-		out.write( "%d %.3f %.3f %.3f %.3f %.3f\n" %(i+1, proba_seg[0], proba_seg[1], proba_seg[2], proba_seg[3], proba_seg[4]) )
-
+    ind_pixs = np.where(f_txt[:, 0] == i + 1)
+    pixs = f_txt[ind_pixs][:, 1:6]
+    n_pixs = pixs.shape[0]
+    if n_pixs == 0:
+        continue
+    else:
+        proba_seg = np.sum(pixs, axis=0) / n_pixs
+        out.write("%d %.3f %.3f %.3f %.3f %.3f\n" % (
+        i + 1, proba_seg[0], proba_seg[1], proba_seg[2], proba_seg[3], proba_seg[4]))
 
 '''
 # Bayesian
--- test/seg2label.py
+++ test/seg2label.py
@@ -5,35 +5,34 @@ from scipy.misc import imsave
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', help = 'prediction over SSPs')
-parser.add_argument('-a', help = 'SSPs image')
-parser.add_argument('-o', help = 'output classif image')
+parser.add_argument('-i', help='prediction over SSPs')
+parser.add_argument('-a', help='SSPs image')
+parser.add_argument('-o', help='output classif image')
 args = parser.parse_args()
 
 f_txt = args.i
 f_img = args.a
 out = args.o
-offset = int(65/2)
+offset = int(65 / 2)
 
 txt = np.loadtxt(f_txt)
 img = scipy.ndimage.imread(f_img)
-img = img[offset:img.shape[0]-offset, offset:img.shape[1]-offset]
-nl,nc = img.shape
+img = img[offset:img.shape[0] - offset, offset:img.shape[1] - offset]
+nl, nc = img.shape
 
-output = np.empty((nl,nc), dtype=int)
+output = np.empty((nl, nc), dtype=int)
 
 # Mean
-for i in range (nl):
-	for j in range(nc):
-		#~ seg_id = img[i,j]+1
-		seg_im = img[i,j]+1
-		seg_id = np.where(txt[:,0]==seg_im)
-		if seg_id[0].shape[0] == 1:
-			seg = txt[seg_id][0,1:6]
-			label = seg.argmax()
-			output[i,j] = label+1
-	print(i)
+for i in range(nl):
+    for j in range(nc):
+        # ~ seg_id = img[i,j]+1
+        seg_im = img[i, j] + 1
+        seg_id = np.where(txt[:, 0] == seg_im)
+        if seg_id[0].shape[0] == 1:
+            seg = txt[seg_id][0, 1:6]
+            label = seg.argmax()
+            output[i, j] = label + 1
+    print(i)
 
 print("toto")
-imsave(out,output)
-
+imsave(out, output)
