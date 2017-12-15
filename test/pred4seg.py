d578a9653ff538d147f267fbed4207700733ca31
tpostadjian <tristanpostadjian@gmail.com>
2017-12-15 18:08:02 +0000

    Merge branch 'stable' of https://github.com/tpostadjian/pytorch into stable
    Conflicts:
    	test/pred4seg.py

--- test/pred4seg.py
+++ test/pred4seg.py
@@ -10,37 +10,49 @@ import h5py
 ratio_pix2class = 0.4
 patch_size = 65
 offset = int(patch_size / 2)
-list_img = glob('tile_16500_38500_seg.tif')
+list_img = glob("../test_set/tile_16500_38500_seg.tif")
+tifDirectory = os.path.abspath(list_img[0])
+h5Directory = os.path.abspath("../test_set")
 count = 0
 
 # Loop over tiles covering the ROI
 for img in list_img:
     tile = os.path.basename(img)
     img_name = tile.split('.')[0]
-    pythonString = "/usr/bin/python2.7 tiff2hdf5_1file.py " + tifDirectory + "/" + img_name + ".tif " + h5Directory + "/" + img_name + ".h5"
+    pythonString = "/usr/bin/python2.7 tiff2hdf5_1file.py " \
+                   + tifDirectory + img \
+                   + h5Directory + "/" + img_name + ".h5"
+    print(pythonString)
     subprocess.call(pythonString)
-    data = h5py.File(h5Directory + "/" + img_name + '.h5')
-    img = data['img_1']
+    data = h5py.File(h5Directory + "/" + img_name + ".h5")
+    img = data["img_1"]
     img_np = np.array(img)
+
     # Reduce boundaries to avoid computing on "no data" areas
     seg = img[offset:img.shape[0] - offset, offset:img.shape[1] - offset]
     nl, nc = seg.shape
     print(nl, nc)
+
     # how many segments ?
     n_s = np.unique(seg)
-    # loop over segments (0.0166s/segment)
+
+    # loop over segments (0.015s/segment)
     start_time = time.time()
     # file to store class probabilities
-    f = open(img_name + '_pred40%.txt', 'w')
+    f = open(img_name + "_pred40%.txt", "w")
+
     print("********************* Classification running *********************")
     for id_seg in n_s:
+
         # retrieve pixels indices for given segment (np.where takes time!)
         seg_ind = np.where(seg == id_seg)
         n_pix_seg = seg_ind[0].shape[0]
-        # randomly pick pixels in the current segment
+
+        # randomly draw pixels within the current segment
         n_pix2class = int(n_pix_seg * ratio_pix2class)
         pix2class = np.random.randint(n_pix_seg, size=n_pix2class)
         ind_pix2class = [seg_ind[0][pix2class], seg_ind[1][pix2class]]
+
         # Classify each picked pixel
         for pix in range(n_pix2class):
             x_pix = ind_pix2class[0][pix]
@@ -50,4 +62,3 @@ for img in list_img:
             count = count + 1
 
     print("******************** Classification: %s seconds ******************" % (time.time() - start_time))
-    print(x_pix * nl + y_pix)
